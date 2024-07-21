d5f40b381207ce608983ca077ba5eed8eb71904b
Mateusz Kobiera <71442804+MateuszKobiera@users.noreply.github.com>
2024-07-21 12:32:10 +0200

    TC1 menu without chains

--- page-models/welcome-page.ts
+++ page-models/welcome-page.ts
@@ -2,19 +2,35 @@ import { type Locator, type Page } from '@playwright/test';
 
 export class WelcomePage {
     readonly page: Page;
+    readonly MenuButton: Locator;
+    readonly MenuMainPageButton: Locator;
+    readonly MenuAvatarButton: Locator;
+    readonly MenuBackofficeButton: Locator;
+    readonly MenuJakTestowacButton: Locator;
     readonly pageHeader: Locator;
     readonly pageHeader2: Locator;
+    readonly GadImage: Locator;
     readonly LetsStartButton: Locator;
     readonly PracticePagesButton: Locator;
     readonly JakTestowacButton: Locator;
+    readonly Footer: Locator;
+    readonly FooterJakTestowacButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.MenuButton = page.getByTestId('menu-main');
+        this.MenuMainPageButton = this.MenuButton.locator('[href="/"]');
+        this.MenuAvatarButton = page.getByTestId('user-dropdown');
+        this.MenuBackofficeButton = page.locator('[href="/tools/backoffice.html"]');
+        this.MenuJakTestowacButton = page.locator('[href="https://jaktestowac.pl"]').locator('[data-hover="Visit jaktestowac.pl"]');
         this.pageHeader = page.getByTestId('pageHeader').locator('h1');
         this.pageHeader2 = page.getByTestId('pageHeader').locator('h2');
+        this.GadImage = page.locator('[src="./data/gad-front-banner.png"]');
         this.LetsStartButton = page.getByTestId('btnGui');
         this.PracticePagesButton = page.getByTestId('btnPractice');
         this.JakTestowacButton = page.locator('div > [href="https://jaktestowac.pl"]').first();
+        this.Footer = page.locator('footer');
+        this.FooterJakTestowacButton = page.locator('footer > div > a[href="https://jaktestowac.pl"]');
     }
 
     async goto() {
--- tests/welcome-page-tests.spec.ts
+++ tests/welcome-page-tests.spec.ts
@@ -3,10 +3,20 @@ import {WelcomePage} from "../page-models/welcome-page";
 
 
 test.describe('TC1 - Welcome Page', () => {
+    const currentYear = new Date().getFullYear();
     test('should be displayed', async ({page}) => {
         const welcomePage = new WelcomePage(page);
         await welcomePage.goto();
 
+        await welcomePage.MenuMainPageButton.click();
+        await expect(page).toHaveURL('http://localhost:3000');
+        // await expect(welcomePage.MenuAvatarButton).toBeVisible();
+        await expect(welcomePage.MenuBackofficeButton).toBeVisible();
+        await expect(welcomePage.MenuJakTestowacButton).toBeVisible();
+        await welcomePage.MenuJakTestowacButton.click();
+        await expect(page).toHaveURL('https://jaktestowac.pl');
+
+        await welcomePage.goto();
         await expect(welcomePage.pageHeader).toHaveText('Welcome on 🦎GUI API Demo');
         await expect(welcomePage.pageHeader2).toHaveText('Explore and create testing content!');
 
@@ -19,6 +29,11 @@ test.describe('TC1 - Welcome Page', () => {
 
         await welcomePage.goto();
         await welcomePage.JakTestowacButton.click();
+        await expect(page).toHaveURL('https://jaktestowac.pl');
+
+        await welcomePage.goto();
+        await expect(welcomePage.Footer).toContainText(`Version: v2.7.1 © Copyright ${currentYear} jaktestowac.pl`);
+        await welcomePage.FooterJakTestowacButton.click();
         await expect(page).toHaveURL('https://jaktestowac.pl');
     });
 });
