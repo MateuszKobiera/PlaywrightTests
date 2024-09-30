6e27c177af838ea5e4ff807da3e3ba7bc8b899b1
Mateusz Kobiera <71442804+MateuszKobiera@users.noreply.github.com>
2024-09-30 23:09:18 +0200

    scratch of test to check for new users. Added functions for repeatedly actions.

--- tests/database-sql-playground-tests.spec.ts
+++ tests/database-sql-playground-tests.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { DatabaseSqlPlaygroundPage } from '../page-models/database-sql-playground';
+import { validUser } from '../data/users';
+import { postRegistration } from '../API/registration';
 
 test.describe('database-sql-playground-tests', () => {
   let dataBasePlaygroundPage: DatabaseSqlPlaygroundPage;
@@ -9,21 +11,35 @@ test.describe('database-sql-playground-tests', () => {
     await page.goto(dataBasePlaygroundPage.url);
   });
 
+  async function runSQLQuery(query: string) {
+    await expect(dataBasePlaygroundPage.SQLQueryEditor).toBeVisible();
+    await expect(dataBasePlaygroundPage.SQLQueryEditorInput).toBeEditable();
+    await dataBasePlaygroundPage.SQLQueryEditorInput.fill(query, { force: true });
+    await dataBasePlaygroundPage.SQLQueryEditorButton.click();
+  }
+
+  async function verifyTableHeaders(expectedHeaders: string[]) {
+    await expect(dataBasePlaygroundPage.Table).toBeVisible();
+    await expect(dataBasePlaygroundPage.TableHeaders).toHaveCount(expectedHeaders.length);
+    for (let i = 0; i < expectedHeaders.length; i++) {
+      await expect(dataBasePlaygroundPage.TableHeaders.nth(i)).toContainText(expectedHeaders[i]);
+    }
+  }
+
+  async function verifyTableRowCount(rowCount: number = undefined) {
+    const count = await dataBasePlaygroundPage.TableRows.count();
+    expect(count).toBeGreaterThan(0);
+    if (rowCount) {
+      expect(count).toBe(rowCount);
+    }
+  }
   test('SQL Query can be run to find users and their articles', async () => {
     const SQL_QUERY =
       'SELECT users.id, CONCAT(firstname, " ", lastname) AS Author, articles.title FROM users \n' +
       'JOIN articles ON articles.user_id = users.id;';
-    await expect(dataBasePlaygroundPage.SQLQueryEditor).toBeVisible();
-    await expect(dataBasePlaygroundPage.SQLQueryEditorInput).toBeEditable();
-    await dataBasePlaygroundPage.SQLQueryEditorInput.fill(SQL_QUERY, { force: true });
-    await dataBasePlaygroundPage.SQLQueryEditorButton.click();
-    await expect(dataBasePlaygroundPage.Table).toBeVisible();
-    await expect(dataBasePlaygroundPage.TableHeaders).toHaveCount(3);
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(0)).toContainText('id');
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(1)).toContainText('Author');
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(2)).toContainText('title');
-    const count = await dataBasePlaygroundPage.TableRows.count();
-    expect(count).toBeGreaterThan(0);
+    await runSQLQuery(SQL_QUERY);
+    await verifyTableHeaders(['id', 'Author', 'title']);
+    await verifyTableRowCount();
   });
 
   test('SQL Query can be run to find users and their likes number grouped by user', async () => {
@@ -32,17 +48,21 @@ test.describe('database-sql-playground-tests', () => {
       '  FROM users\n' +
       '  JOIN likes ON users.id = likes.user_id\n' +
       '  GROUP BY users.id, users.firstname, users.lastname;';
-    await expect(dataBasePlaygroundPage.SQLQueryEditor).toBeVisible();
-    await expect(dataBasePlaygroundPage.SQLQueryEditorInput).toBeEditable();
-    await dataBasePlaygroundPage.SQLQueryEditorInput.fill(SQL_QUERY, { force: true });
-    await dataBasePlaygroundPage.SQLQueryEditorButton.click();
-    await expect(dataBasePlaygroundPage.Table).toBeVisible();
-    await expect(dataBasePlaygroundPage.TableHeaders).toHaveCount(4);
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(0)).toContainText('id');
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(1)).toContainText('firstname');
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(2)).toContainText('lastname');
-    await expect(dataBasePlaygroundPage.TableHeaders.nth(3)).toContainText('Number_of_given_likes');
-    const count = await dataBasePlaygroundPage.TableRows.count();
-    expect(count).toBeGreaterThan(0);
+    await runSQLQuery(SQL_QUERY);
+    await verifyTableHeaders(['id', 'firstname', 'lastname', 'Number_of_given_likes']);
+    await verifyTableRowCount();
+  });
+
+  test('SQL Query can be run to find new users', async ({ request }) => {
+    const SQL_QUERY = "SELECT * FROM users WHERE email = 'test@test.pl'";
+
+    const newUser = validUser;
+
+    const { response } = await postRegistration(request, newUser);
+    expect(response.status()).toBe(201);
+
+    await runSQLQuery(SQL_QUERY);
+    await verifyTableHeaders(['firstname', 'lastname', 'email', 'password', 'avatar', 'id (PK)']);
+    await verifyTableRowCount(1);
   });
 });
